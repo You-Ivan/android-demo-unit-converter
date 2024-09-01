b78c0408247197e718e9e8b926899dd28e58d7e4
Ivan You <yuanfanyou@gmail.com>
2024-09-01 09:13:06 -0700

    add input text field,dropdown menu and spacer

--- UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -3,10 +3,29 @@ package com.example.unitconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +38,66 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    UnitConverter()
                 }
             }
         }
     }
+}
+
+val UNIT_OPTIONS = listOf("Meter", "Centimeter", "Inch", "Feet")
+
+@Composable
+fun UnitConverter() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row {
+            Text(text = "Unit Converter")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row {
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                placeholder = { Text(text = "Place enter a number") })
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row {
+            DropdownBtn("Select", UNIT_OPTIONS)
+            Spacer(modifier = Modifier.width(16.dp))
+            DropdownBtn("Select", UNIT_OPTIONS)
+
+        }
+    }
+}
+
+@Composable
+fun DropdownBtn(description: String, options: List<String>) {
+    Box {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = description)
+            Icon(
+                imageVector = Icons.Default.ArrowDropDown,
+                contentDescription = "Arrow Down"
+            )
+            DropdownMenu(expanded = true, onDismissRequest = { /*TODO*/ }) {
+                options.forEach {
+                    DropdownMenuItem(
+                        text = { Text(text = it) },
+                        onClick = { /*TODO*/ })
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun PreviewUnitConverter() {
+    UnitConverter()
 }
 
 
